fd377bd8666d8c37c4368464f7dec8640361e3ea
Clashix <84671657+Clashix@users.noreply.github.com>
2021-12-22 17:06:11 +0100

    En français

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Coming Soon...</p>
+        <p>Prochainement...
+        
+        </p>
       </header>
     </div>
   );
