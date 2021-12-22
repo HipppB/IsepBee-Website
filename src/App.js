24838fe5de7ee81180a1097832fea664cf1a5083
Hippolyte Bach <hbach@juniorisep.com>
2021-12-22 16:50:57 +0100

    Basis Test INVALID

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from "./assets/isepToken.png";
 import "./App.css";
+function Test() {
+  return <p>test</p>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Coming Soon...</p>
+        <p>Coming Soon...</p>kodzidoqjzdioqzjdioqzjdioqzjdioqzd
       </header>
     </div>
   );
