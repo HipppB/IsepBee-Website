8114e08f0f311b6352437651a6403b6afb37657a
Hippolyte Bach <hbach@juniorisep.com>
2022-04-04 18:09:36 +0200

    flexChart

--- src/components/graph/piechart.js
+++ src/components/graph/piechart.js
@@ -5,26 +5,33 @@ import "../../style/wallet.css";
 
 const piechart = () => {
   return (
-    <div className="pie">
-      <VictoryPie
-        colorScale={["tomato", "orange", "gold"]}
-        data={[
-          { x: " ", y: 50 },
-          { x: " ", y: 50 },
-          { x: " ", y: 100 },
-        ]}
-      />
-      <VictoryLegend
-        x={350}
-        y={-15}
-        title="Legend"
-        centerTitle
-        orientation="vertical"
-        gutter={20}
-        colorScale={["tomato", "orange", "gold"]}
-        style={{ border: { stroke: "black" }, title: { fontSize: 20 } }}
-        data={[{ name: "Tomato" }, { name: "orange" }, { name: "gold" }]}
-      />
+    <div
+      className="pie"
+      style={{ backgroundColor: "red", display: "flex", flexDirection: "row" }}
+    >
+      <div style={{ backgroundColor: "green" }}>
+        <VictoryPie
+          colorScale={["tomato", "orange", "gold"]}
+          data={[
+            { x: " ", y: 50 },
+            { x: " ", y: 50 },
+            { x: " ", y: 100 },
+          ]}
+        />
+      </div>
+      <div style={{ backgroundColor: "yellow" }}>
+        <VictoryLegend
+          // x={350}
+          y={-15}
+          title="Legend"
+          centerTitle
+          orientation="vertical"
+          gutter={20}
+          colorScale={["tomato", "orange", "gold"]}
+          style={{ border: { stroke: "black" }, title: { fontSize: 20 } }}
+          data={[{ name: "Tomato" }, { name: "orange" }, { name: "gold" }]}
+        />
+      </div>
     </div>
   );
 };
