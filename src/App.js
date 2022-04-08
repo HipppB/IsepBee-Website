9adf9b0bf6279f1c881e86fe6a4a8eb81e31632e
Clashix <84671657+Clashix@users.noreply.github.com>
2022-04-08 16:34:28 +0200

    debut

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/footer.js";
 import NewsLetter from "./components/newsletter.js";
 import Performance from "./components/performance.js";
 import Wallet from "./components/wallet.js";
+import Chart from "./components/graph/chart.js";
 
 function App() {
   return (
@@ -16,11 +17,27 @@ function App() {
       <Roadmap />
       <Performance />
       <Wallet />
+      <div style={{ marginLeft: "800px", marginTop: "50px" }}>
+        <Chart data={initialData}></Chart>{" "}
+      </div>
       <NewsLetter />
       <Footer />
       <br />
     </div>
   );
 }
+
+const initialData = [
+  { time: "2018-12-22", value: 32.51 },
+  { time: "2018-12-23", value: 31.11 },
+  { time: "2018-12-24", value: 27.02 },
+  { time: "2018-12-25", value: 27.32 },
+  { time: "2018-12-26", value: 25.17 },
+  { time: "2018-12-27", value: 28.89 },
+  { time: "2018-12-28", value: 25.46 },
+  { time: "2018-12-29", value: 23.92 },
+  { time: "2018-12-30", value: 22.68 },
+  { time: "2018-12-31", value: 22.67 },
+];
 
 export default App;
---
+++ src/components/graph/chart.js
@@ -0,0 +1,34 @@
+import { createChart } from "lightweight-charts";
+import React, { useEffect, useRef } from "react";
+
+const ChartComponent = (props) => {
+  const chartContainerRef = useRef();
+
+  useEffect(() => {
+    const handleResize = () => {
+      chart.applyOptions({ width: chartContainerRef.current.clientWidth });
+    };
+
+    const { data } = props;
+
+    const chart = createChart(chartContainerRef.current, {
+      width: chartContainerRef.current.clientWidth,
+      height: 300,
+    });
+    chart.timeScale().fitContent();
+
+    const newSeries = chart.addAreaSeries();
+    newSeries.setData(data);
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+
+      chart.remove();
+    };
+  }, [props.data]);
+
+  return <div ref={chartContainerRef} />;
+};
+
+export default ChartComponent;
