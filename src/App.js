db5815ef18e92d007c3288d7dffb61e2faff79a8
Clashix <84671657+Clashix@users.noreply.github.com>
2022-04-25 15:29:28 +0200

    magnifique

--- src/App.js
+++ src/App.js
@@ -8,11 +8,26 @@ import NewsLetter from "./components/newsletter.js";
 import Performance from "./components/performance.js";
 import Wallet from "./components/wallet.js";
 import Chart from "./components/graph/chart.js";
-
+import react, { useState, useEffect } from "react";
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return width;
+}
 function App() {
+  const [width, setWidth] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    window.addEventListener("resize", () => setWidth(getWindowDimensions()));
+    return () =>
+      window.removeEventListener("resize", () =>
+        setWidth(getWindowDimensions())
+      );
+  });
+
+  useEffect(() => console.log(width));
   return (
     <div className="App">
-      <HeaderTest />
+      {width > 500 && <HeaderTest />}
       <Title />
       <Roadmap />
       <Performance />
