9fe59720b0d3fc27c8255ecb53bc9fd0f11cfe91
Clashix <84671657+Clashix@users.noreply.github.com>
2022-04-26 16:22:14 +0200

    a

--- src/components/header/header500.js
+++ src/components/header/header500.js
@@ -1,24 +1,30 @@
 import logo from "../../assets/isepToken.png";
 import "../../style/header.css";
-
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
 function header500() {
   return (
-    <div className="headerContainer">
-      <a href="" className="logo">
-        <img
-          src={logo}
-          width="75"
-          height="72"
-          alt="Logo"
-          className="logo_image"
-        />
-      </a>
-      <div className="linkContainer">
-        <a href="#about">About</a>
-        <a href="#ourgoal">Our goal</a>
-        {/* <a href="#whitepaper">Whitepaper</a> */}
-        <a href="#roadmap">Roadmap</a>
-        <a href="#contact">Contact</a>
+    <div class="dropdown">
+      <button onclick="myFunction()" className="dropbtn">
+        Dropdown
+      </button>
+      <div id="myDropdown" className="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
       </div>
     </div>
   );
