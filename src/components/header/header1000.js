05085ab1a9d0fb6bffa7354ddeec15a49a010343
Clashix <84671657+Clashix@users.noreply.github.com>
2022-04-30 12:49:27 +0200

    changement

--- src/components/header/header1000.js
+++ src/components/header/header1000.js
@@ -3,22 +3,24 @@ import "../../style/header.css";
 
 function Header1000() {
   return (
-    <div className="headerContainer">
-      <a href="" className="logo" style={{ justifyContent: "center" }}>
-        <img
-          src={logo}
-          width="75"
-          height="72"
-          alt="Logo"
-          className="logo_image"
-        />
-      </a>
-      <div className="linkContainer">
-        <a href="#about">About</a>
-        <a href="#ourgoal">Our goal</a>
-        <a href="#whitepaper">Whitepaper</a>
-        <a href="#roadmap">Roadmap</a>
-        <a href="#contact">Contact</a>
+    <div>
+      <div className="headerContainer">
+        <a href="" className="logo" style={{ justifyContent: "center" }}>
+          <img
+            src={logo}
+            width="75"
+            height="72"
+            alt="Logo"
+           className="logo_image"
+          />
+        </a>
+        <div className="linkContainer">
+          <a href="#about">About</a>
+          <a href="#ourgoal">Our goal</a>
+          <a href="#whitepaper">Whitepaper</a>
+          <a href="#roadmap">Roadmap</a>
+          <a href="#contact">Contact</a>
+        </div>
       </div>
     </div>
   );
