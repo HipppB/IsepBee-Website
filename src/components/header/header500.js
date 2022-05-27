81dc9882c557970992b3b47ac6592c458a314ecc
Clashix <84671657+Clashix@users.noreply.github.com>
2022-05-27 12:31:41 +0200

    test

--- src/components/header/header500.js
+++ src/components/header/header500.js
@@ -2,12 +2,26 @@ import React, { useState, useEffect } from "react";
 import logo from "../../assets/isepToken.png";
 import "../../style/header.css";
 
+const hamburgerToggler = document.querySelector(".hamburger");
+const navLinksContainer = document.querySelector(".navlinks-container");
+
+const toggleNav = () => {
+  hamburgerToggler.classList.toggle("open")
+
+  const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ?  "false" : "true";
+  hamburgerToggler.setAttribute("aria-expanded", ariaToggle)
+
+  navLinksContainer.classList.toggle("open")
+}
+// hamburgerToggler.classList.toggle("click", toggleNav);
+
+
 function Header500() {
-  const [isOpen, setOpen] = useState(false);
-  function myFunction() {
-    console.log(isOpen);
-    setOpen(!isOpen);
-  }
+  // const [isOpen, setOpen] = useState(false);
+  // function myFunction() {
+  //   console.log(isOpen);
+  //   setOpen(!isOpen);
+  // }
   return (
     <div style={{ width: "100%", paddingTop: 10 }}>
       <div
@@ -27,19 +41,21 @@ function Header500() {
           />
         </a>
 
-        <button onClick={() => myFunction()} className="dropbtn">
-          Dropdown
+        <div class="main-navlinks">
+        <button class="hamburger" type="button" aria-label="Toggle navigation" aria-expanded="false">
+          <span></span>
+          <span></span>
+          <span></span>
         </button>
-      </div>
-      {isOpen && (
-        <div className="dropdown-content">
+        <div class="navlinks-container">
           <a href="#about">About</a>
           <a href="#ourgoal">Our goal</a>
           <a href="#whitepaper">Whitepaper</a>
           <a href="#roadmap">Roadmap</a>
           <a href="#contact">Contact</a>
         </div>
-      )}
+    </div>
+    </div>
     </div>
   );
 }
